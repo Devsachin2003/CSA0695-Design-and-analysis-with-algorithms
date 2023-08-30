90a6710fa66841bba922fde4a600582000911642
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-08-30 09:22:03 +0530

    Add files via upload

---
+++ permutation of an array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void permute(int arr[], int start, int end) {
+    if (start == end) {
+        for (int i = 0; i <= end; i++) {
+            printf("%d", arr[i]);
+            if (i != end)
+                printf(", ");
+        }
+        printf("\n");
+    } else {
+        for (int i = start; i <= end; i++) {
+            swap(&arr[start], &arr[i]);
+            permute(arr, start + 1, end);
+            swap(&arr[start], &arr[i]); // Backtrack
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Output:\n");
+    permute(arr, 0, n - 1);
+
+    return 0;
+}
